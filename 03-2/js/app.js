b499d5d8dbb4051050499c5fcc0e6e17bf09d100
soominJung0413 <doli061214@gmail.com>
2020-09-24 21:10:23 +0900

    VueRouter
    :)

---
+++ 03-2/js/app.js
@@ -0,0 +1,19 @@
+
+Vue.component('todo-footer',{
+  template: '<p>This is another global child component</p>'
+});
+
+
+var appAttr = {
+  template: '<p>This is another local child component</p>'
+};
+
+var app = new Vue({
+  el:'#app',
+  data:{
+    message : 'This is a parent Compoent'
+  },
+  components:{
+    'todo-list':appAttr
+  }
+});
