2977f9b689e34f1c02b20b81ce88cfc367cedd43
soominJung0413 <doli061214@gmail.com>
2020-10-30 00:03:23 +0900

    image
    .

--- Start_Vue_App/src/components/TodoInput.vue
+++ Start_Vue_App/src/components/TodoInput.vue
@@ -1,7 +1,12 @@
 <template lang="html">
   <div class="inputBox shadow">
     <label>
-      <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+      <input
+        type="text"
+        v-model="newTodoItem"
+        placeholder="Type what you have to do"
+        v-on:keyup.enter="addTodo"
+      />
     </label>
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
@@ -11,66 +16,66 @@
       <h3 slot="header">경고</h3>
       <span slot="body"></span>
       <span slot="footer" @click="showModal = false">
-      할 일 을 입력하세요
-      <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+        할 일 을 입력하세요
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
       </span>
     </modal>
   </div>
 </template>
 
 <script>
-  import Modal from './common/Modal.vue'
+import Modal from "./common/Modal.vue";
 
 export default {
   data() {
-      return {
-          newTodoItem : '',
-          showModal : false
-      }
+    return {
+      newTodoItem: "",
+      showModal: false
+    };
   },
-  methods : {
-      addTodo(){
-         if(this.newTodoItem !== ""){
-             var value = this.newTodoItem && this.newTodoItem.trim();
-              this.$emit("addTodo",value);
-            this.clearInput();
-         } else {
-             this.showModal = !this.showModal;
-         }
-      },
-          clearInput(){
-              this.newTodoItem = '';
-          }
-      },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        this.$emit("addTodo", value);
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    }
+  },
   components: {
-      Modal : Modal
+    Modal: Modal
   }
-}
+};
 </script>
 
 <style lang="css" scoped>
-  input:focus{
-    outline: none;
-  }
-  .inputBox{
-    background: white;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 5px;
-  }
-  .inputBox input {
-    border-style: none;
-    font-size: 0.9rem;
-  }
-  .addContainer{
-    float: right;
-    background: linear-gradient(to right,#6478FB, #8763FB);
-    display: block;
-    width: 3rem;
-    border-radius: 0 5px 5px 0;
-  }
-  .addBtn{
-    color: white;
-    vertical-align: middle;
-  }
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
